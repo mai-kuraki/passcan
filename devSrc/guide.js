bebbd3a808de46295651d822869a8ffec54d8156
maikuraki <zhengliuyang@huanqiu.com>
2018-08-23 14:49:50 +0800

    update

--- devSrc/guide.js
+++ devSrc/guide.js
@@ -16,7 +16,7 @@ class Guide {
             setTimeout(() => {
                 this.loading(false);
             }, 4500);
-            chrome.webstore.install('https://chrome.google.com/webstore/detail/imamemhokkdleoelohnmkimbmpfglcil', () => {
+            chrome.webstore.install('https://chrome.google.com/webstore/detail/ilbddbbikgbfohimdbkegcminkeagpao', () => {
                 console.log('安装成功');
                 this.status = false;
                 this.handleInstall();
